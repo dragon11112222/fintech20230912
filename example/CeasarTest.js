03b7e5652ba1eba9af2afe5819221e7611e255b7
Seyong JUNG <40597818+dragon11112222@users.noreply.github.com>
2023-09-19 10:33:16 +0900

    카이사르 암호화 2
    기존 파일은 출력된 암호문을 손으로 복붙하는 방식이었으나, 이번엔 암호문이 저장된 객체를 그대로 복호화 함수 안에 받아서 암호문 일일이 복붙할 필요 없음.

--- example/CeasarTest.js
+++ example/CeasarTest.js
@@ -1,6 +1,7 @@
 const cipher = require("./Ceasar");
 const encData = cipher.encrypt(3, "hi how are you i am fine");
 //kl krz duh brx l dp ilqh
-const decData = cipher.decrypt(3, "kl krz duh brx l dp ilqh");
+// const decData = cipher.decrypt(3, "kl krz duh brx l dp ilqh");
+const decData = cipher.decrypt(3, encData);
 console.log(encData);
 console.log(decData);
