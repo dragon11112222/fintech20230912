bcad31b806635f24fd50a4e73d87ef43a8d15763
Seyong JUNG <40597818+dragon11112222@users.noreply.github.com>
2023-09-28 12:29:30 +0900

    양방향 해쉬 함수
    crypto.createCipheriv(algorithm, key, iv);
    createDecipheriv(algorithm, key, iv);

--- example/Enc.js
+++ example/Enc.js
@@ -11,3 +11,31 @@ const sha256Enc = (plainText, key) => {
 };
 
 console.log(sha256Enc("1234", "fintech"));
+
+
+// 양방향
+const AESEncrypt = (plainText) => {
+  const algorithm = 'aes-256-cbc';
+  const key = 'H3uVnnOnNIXXsH63CsgswUsmICSCdVsn';
+  const iv = 'dwUgwmwOndnJOmOX';
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(plainText, "utf8", "base64");
+  encrypted += cipher.final("base64");
+  console.log(encrypted);
+  return encrypted;
+};
+
+const AESDecrypt = (plainText) => {
+  const algorithm = 'aes-256-cbc';
+  const key = 'H3uVnnOnNIXXsH63CsgswUsmICSCdVsn';
+  const iv = 'dwUgwmwOndnJOmOX';
+  const decipher = createDecipheriv(algorithm, key, iv);
+  let decrypted = cipher.update(plainText, "base64", "utf8");
+  decrypted += cipher.final('utf8');
+  console.log(decrypted);
+  return decrypted;
+};
+
+
+AESEncrypt('!Kwic123테스트');
+// AESDecrypt('');
