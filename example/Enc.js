4928d3fb547f7d629f0fa182807aed955e8e411e
Seyong JUNG <40597818+dragon11112222@users.noreply.github.com>
2023-09-28 12:39:11 +0900

    양방향 해쉬 함수 2 (decrypt 관련 오류 수정)
    decrypt 관련 오류 수정 및 결과물 확인
    - createDecipheriv(); 앞에 crypto. 붙여주고,
    - decrypted 객체 정의 시 cipher를 decipher로 변경.
    - 마지막으로 AESDecrypt(''); 실행.

--- example/Enc.js
+++ example/Enc.js
@@ -29,13 +29,13 @@ const AESDecrypt = (plainText) => {
   const algorithm = 'aes-256-cbc';
   const key = 'H3uVnnOnNIXXsH63CsgswUsmICSCdVsn';
   const iv = 'dwUgwmwOndnJOmOX';
-  const decipher = createDecipheriv(algorithm, key, iv);
-  let decrypted = cipher.update(plainText, "base64", "utf8");
-  decrypted += cipher.final('utf8');
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(plainText, "base64", "utf8");
+  decrypted += decipher.final('utf8');
   console.log(decrypted);
   return decrypted;
 };
 
 
 AESEncrypt('!Kwic123테스트');
-// AESDecrypt('');
+AESDecrypt('b3Bu7QBZ0q96+f2GY2NQmaT1XnZElI1+wRdSHBOY01s=');
