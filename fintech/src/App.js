f9d922c5bb8027529eaa9fb115ac3d422bd3ce04
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-11-12 22:53:57 +0900

    헤더, 리스트, 스타일 적용된 App.js

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-// import Welcome from './components/Welcome.js';
-// import StateComponent from './components/StateComponent.js';
-// import EventComponent from './components/EventComponent.js';
-// import ListComponent from './components/ListComponent.js';
-// import StyledComponent from './components/StyledComponent.js';
-//
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <Welcome username = '정세용' age = {30}></Welcome>
-//       <StateComponent></StateComponent>
-//       <EventComponent></EventComponent>
-//       <ListComponent></ListComponent>
-//       <StyledComponent></StyledComponent>
-//     </div>
-//   );
-// }
-//
-// export default App;
+import Welcome from './components/Welcome.js';
+import StateComponent from './components/StateComponent.js';
+import EventComponent from './components/EventComponent.js';
+import ListComponent from './components/ListComponent.js';
+import StyledComponent from './components/StyledComponent.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome username = '정세용' age = {30}></Welcome>
+      <StateComponent></StateComponent>
+      <EventComponent></EventComponent>
+      <ListComponent></ListComponent>
+      <StyledComponent></StyledComponent>
+    </div>
+  );
+}
+
+export default App;
 
 
 
@@ -47,29 +47,29 @@ import './App.css';
 
 
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from './pages/MainPage';
-import AuthResult from './pages/AuthResult';
-import AccountList from './pages/AccountList';
-import Balance from "./pages/Balance";
-import QrCode from "./pages/QrCode";
-import QrReader from "./pages/QrReader";
-import Login from "./pages/Login";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage></MainPage>}></Route>
-        <Route path="/authResult" element={<AuthResult></AuthResult>}></Route>
-        <Route path="/list" element={<AccountList></AccountList>}></Route>
-        <Route path="/balance" element={<Balance />}></Route>
-        <Route path="/qrcode" element={<QrCode />}></Route>
-        <Route path="/qrreader" element={<QrReader />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import MainPage from './pages/MainPage';
+// import AuthResult from './pages/AuthResult';
+// import AccountList from './pages/AccountList';
+// import Balance from "./pages/Balance";
+// import QrCode from "./pages/QrCode";
+// import QrReader from "./pages/QrReader";
+// import Login from "./pages/Login";
+//
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<MainPage></MainPage>}></Route>
+//         <Route path="/authResult" element={<AuthResult></AuthResult>}></Route>
+//         <Route path="/list" element={<AccountList></AccountList>}></Route>
+//         <Route path="/balance" element={<Balance />}></Route>
+//         <Route path="/qrcode" element={<QrCode />}></Route>
+//         <Route path="/qrreader" element={<QrReader />}></Route>
+//         <Route path="/login" element={<Login />}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+//
+// export default App;
