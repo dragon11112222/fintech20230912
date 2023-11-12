17426b8eb773b3591665d8fb9149536b96472cd8
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-11-12 23:26:28 +0900

    사용자 인증 페이지 이동 작성 시작

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -24,52 +24,52 @@ import './App.css';
 
 
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ListComponent from './components/ListComponent';
-import HeaderComponent from './components/HeaderComponent';
-import AxiosComponent from './components/AxiosComponent';
-import NewsSearch from './pages/NewsSearch';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ListComponent></ListComponent>}></Route>
-        <Route path='/header' element = {<HeaderComponent></HeaderComponent>}></Route>
-        <Route path="/axios" element={<AxiosComponent></AxiosComponent>}></Route>
-        <Route path="/news" element={<NewsSearch></NewsSearch>}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-
-
-
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import MainPage from './pages/MainPage';
-// import AuthResult from './pages/AuthResult';
-// import AccountList from './pages/AccountList';
-// import Balance from "./pages/Balance";
-// import QrCode from "./pages/QrCode";
-// import QrReader from "./pages/QrReader";
-// import Login from "./pages/Login";
+// import ListComponent from './components/ListComponent';
+// import HeaderComponent from './components/HeaderComponent';
+// import AxiosComponent from './components/AxiosComponent';
+// import NewsSearch from './pages/NewsSearch';
 //
 // function App() {
 //   return (
 //     <BrowserRouter>
 //       <Routes>
-//         <Route path="/" element={<MainPage></MainPage>}></Route>
-//         <Route path="/authResult" element={<AuthResult></AuthResult>}></Route>
-//         <Route path="/list" element={<AccountList></AccountList>}></Route>
-//         <Route path="/balance" element={<Balance />}></Route>
-//         <Route path="/qrcode" element={<QrCode />}></Route>
-//         <Route path="/qrreader" element={<QrReader />}></Route>
-//         <Route path="/login" element={<Login />}></Route>
+//         <Route path="/" element={<ListComponent></ListComponent>}></Route>
+//         <Route path='/header' element = {<HeaderComponent></HeaderComponent>}></Route>
+//         <Route path="/axios" element={<AxiosComponent></AxiosComponent>}></Route>
+//         <Route path="/news" element={<NewsSearch></NewsSearch>}></Route>
 //       </Routes>
 //     </BrowserRouter>
 //   );
 // }
 //
 // export default App;
+
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from './pages/MainPage';
+import AuthResult from './pages/AuthResult';
+import AccountList from './pages/AccountList';
+import Balance from "./pages/Balance";
+import QrCode from "./pages/QrCode";
+import QrReader from "./pages/QrReader";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage></MainPage>}></Route>
+        <Route path="/authResult" element={<AuthResult></AuthResult>}></Route>
+        <Route path="/list" element={<AccountList></AccountList>}></Route>
+        <Route path="/balance" element={<Balance />}></Route>
+        <Route path="/qrcode" element={<QrCode />}></Route>
+        <Route path="/qrreader" element={<QrReader />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
