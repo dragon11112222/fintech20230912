8c9fbaf06f7c8bcb168c275f554d31c900fd463b
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-11-13 00:03:45 +0900

    dotenv 설정 및 커넥션 설정 완료
    npm install mysql2
    npm install dotenv

---
+++ fintech_server/mysqlTest.js
@@ -0,0 +1,31 @@
+// get the client
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_ACCOUNT,
+  password: process.env.DB_PASSWORD,
+  database: 'fintech'
+});
+
+// simple query
+connection.query(
+  'SELECT user_name FROM user',
+  function(err, results, fields) {
+    console.log(err);
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+
+// with placeholder
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+  ['Page', 45],
+  function(err, results) {
+    console.log(results);
+  }
+);
